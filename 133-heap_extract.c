cfec67268887baf7e5683ba01f1fa68e64b95419
simoonge1 <simoimagine@gmail.com>
2024-01-04 01:35:15 -0800

    c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -7,24 +7,25 @@
  */
 heap_t *max(heap_t *tree)
 {
-	heap_t *curr_max, *left_max, *right_max;
+    heap_t *curr_max, *left_max, *right_max;
 
-	if (!tree->left)
-		return (tree);
-	left_max = max(tree->left);
-	if (left_max->n > tree->n)
-		curr_max = left_max;
-	else
-		curr_max = tree;
-	if (tree->right)
-	{
-		right_max = max(tree->right);
-		if (right_max->n > curr_max->n)
-			curr_max = right_max;
-		else
-			curr_max = tree;
-	}
-	return (curr_max);
+    if (!tree->left)
+        return tree;
+    
+    left_max = max(tree->left);
+    
+    if (left_max->n > tree->n)
+        curr_max = left_max;
+    else
+        curr_max = tree;
+
+    if (tree->right)
+    {
+        right_max = max(tree->right);
+        if (right_max->n > curr_max->n)
+            curr_max = right_max;
+    }
+    return curr_max;
 }
 
 /**
@@ -33,26 +34,32 @@ heap_t *max(heap_t *tree)
  */
 void recurse_extract(heap_t *tree)
 {
-	heap_t *sub_max, *right_max = NULL;
+    heap_t *sub_max, *right_max = NULL;
 
-	if (!tree->left)
-		return;
-	sub_max = max((tree)->left);
-	if (tree->right)
-		right_max = max(tree->right);
-	if (right_max && right_max->n > sub_max->n)
-		sub_max = right_max;
-	tree->n = sub_max->n;
-	if (!sub_max->left)
-	{
-		if (sub_max->parent && sub_max->parent->left == sub_max)
-			sub_max->parent->left = NULL;
-		if (sub_max->parent && sub_max->parent->right == sub_max)
-			sub_max->parent->right = NULL;
-		free(sub_max);
-	}
-	else
-		recurse_extract(sub_max);
+    if (!tree->left)
+        return;
+
+    sub_max = max(tree->left);
+    
+    if (tree->right)
+        right_max = max(tree->right);
+
+    if (right_max && right_max->n > sub_max->n)
+        sub_max = right_max;
+
+    tree->n = sub_max->n;
+
+    if (!sub_max->left)
+    {
+        if (sub_max->parent && sub_max->parent->left == sub_max)
+            sub_max->parent->left = NULL;
+        else if (sub_max->parent && sub_max->parent->right == sub_max)
+            sub_max->parent->right = NULL;
+
+        free(sub_max);
+    }
+    else
+        recurse_extract(sub_max);
 }
 
 /**
@@ -62,18 +69,20 @@ void recurse_extract(heap_t *tree)
  */
 int heap_extract(heap_t **root)
 {
-	int value;
+    int value;
 
-	if (!*root)
-		return (0);
-	value = (*root)->n;
-	if (!(*root)->left)
-	{
-		value = (*root)->n;
-		free(*root);
-		*root = NULL;
-		return (value);
-	}
-	recurse_extract(*root);
-	return (value);
+    if (!root || !*root)
+        return 0;
+
+    value = (*root)->n;
+
+    if (!(*root)->left)
+    {
+        free(*root);
+        *root = NULL;
+        return value;
+    }
+
+    recurse_extract(*root);
+    return value;
 }
